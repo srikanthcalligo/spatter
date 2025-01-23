503253bcb2ac03237a31ea13a8044c3fb35c1159
Eswaraiah Gorla <eswaraiah.gorla@calligotech.com>
2025-01-23 16:08:22 +0530

    Spatter

---
+++ build_serial/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/user/cmake_3_30_6_version/spatter
+# Build directory: /home/user/cmake_3_30_6_version/spatter/build_serial
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("_deps/nlohmann_json-build")
+subdirs("src")
+subdirs("tests")
